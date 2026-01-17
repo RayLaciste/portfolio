4b8bc3c69c112cca2db8807a339d09e004aef483
RayLaciste <67614706+RayLaciste@users.noreply.github.com>
2026-01-16 16:27:37 -0800

    (feat) Add Navbar
    - import public assets
    - import index.css
    - import constants.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "#components/Navbar";
+
 const App = () => {
-  return <div>Hello, World!</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
